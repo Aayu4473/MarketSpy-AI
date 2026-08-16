a7ae32b71c47453448b0e4b3467986f85bcb58cc
Aayu4473 <aayushi.tiwari2019@gmail.com>
2026-08-16 20:13:21 +0530

    competitor.py

--- backend/app/crud/competitor.py
+++ backend/app/crud/competitor.py
@@ -10,7 +10,7 @@ from app.schemas.competitor import CompetitorCreate
 # 🚀 1. CREATE: Unpacks ALL fields from the schema and saves them to the vault
 async def create_competitor(db: AsyncSession, competitor_in: CompetitorCreate) -> Competitor:
                                              # VIP PASS having valid data from schemas bouncer
-#The Manager then physically unpacks the data from the VIP pass (competitor_in) and 
+#The Manager then manually unpacks the data from the VIP pass (competitor_in) and 
 # places it into the metal box (db_obj):
 
 #Take the name from the VIP pass ➔ Put it in the box's name slot.
@@ -23,14 +23,13 @@ async def create_competitor(db: AsyncSession, competitor_in: CompetitorCreate) -
     await db.refresh(db_obj)
     return db_obj
 
-# 🚀 2. READ (ALL): Fetch a complete list of tracked companies
+# 2. READ (ALL): Fetch a complete list of tracked companies
 async def get_competitors(db: AsyncSession, skip: int = 0, limit: int = 100):
     # Construct a selective SQL query with pagination boundaries
     statement = select(Competitor).offset(skip).limit(limit)
     result = await db.execute(statement)
     return result.scalars().all() # Return as a clean Python list
 
-# 🚀 3. READ (BY ID): Look up one exact company
 async def get_competitor_by_id(db: AsyncSession, competitor_id: int) -> Competitor | None:
     statement= select(Competitor).where(Competitor.id== competitor_id)
     result= await db.execute(statement)
